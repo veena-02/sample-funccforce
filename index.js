c23fa872b1ee2a8c1d1247ff13eeadcb8c532f03
Veena-02 <veenachaddha1602@outlook.com>
2021-01-20 23:52:46 +0530

    updated

---
+++ index.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "400px";
+  document.getElementById("main").style.marginright = "400px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginright= "0";
+}
